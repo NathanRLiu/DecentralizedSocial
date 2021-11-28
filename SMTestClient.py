05b64dc13fcf911f711715654dbe0625e22d6454
Nathan R. Liu <nathanliu17@gmail.com>
2021-11-28 13:50:56 -0800

    add endpoint to ask node for other nodes

--- SMTestClient.py
+++ SMTestClient.py
@@ -1,9 +1,26 @@
 import socket
 
-desiredIP = input("Please enter node IP")
+#desiredIP = input("Please enter node IP")
+desiredIP = "localhost"
 desiredPort = 3000
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mySocket:
     mySocket.connect((desiredIP, desiredPort))
-    mySocket.sendall((1).to_bytes(1,'little'))
-    data = mySocket.recv(1024)
-print("Received: " + data.decode("utf-8"))
+    counter = 0
+    while (True):
+        counter = 0
+        userChoice = input("Go to next post (n), previous post(p), or save(s), or TESTCASE(t)")
+        if (userChoice == "n"):
+            counter += 1
+            mySocket.sendall((counter).to_bytes(1,'little'))
+        elif (userChoice == "p"):
+            counter -= 1
+            mySocket.sendall((counter).to_bytes(1,'little'))
+        elif (userChoice == "s"):
+            #save the current post
+            pass
+        elif (userChoice == "t"):
+            mySocket.sendall(("a34").encode('utf-8'))
+        else:
+            mySocket.sendall(userChoice.encode('utf-8'))
+        data = mySocket.recv(1024)
+        print("Received: " + data.decode("utf-8"))
--- SMTestServer.py
+++ SMTestServer.py
@@ -1,8 +1,11 @@
 import socket
-ServerIP = input("Please enter desired host IP address")
-ServerPort = int(input("Please enter desired host port"))
+#ServerIP = input("Please enter desired host IP address")
+#ServerPort = int(input("Please enter desired host port"))
+ServerIP = "localhost"
+ServerPort = 3000
 
 suggestPosts = ["post 1", "post 2", "post 3"]
+knownNodes = []
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mySocket:
     mySocket.bind((ServerIP, ServerPort))
@@ -10,8 +13,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mySocket:
     conn, addr = mySocket.accept()
     with conn:
         print('Connected to: ' , addr)
+        knownNodes.append(addr)
         while True:
             data = conn.recv(1024)
-            requestedNum = int.from_bytes(data, 'little', signed = False)
-            print(requestedNum)
-            conn.sendall(suggestPosts[requestedNum].encode('utf-8'))
+            payload = data.decode('utf-8')
+            if (payload[0] == "a"):
+                myIndex = int(payload[1:])
+                if (myIndex <= len(knownNodes)):
+                    print(knownNodes[myIndex])
+                    conn.sendall(knownNodes[myIndex][0].encode('utf-8'))
+                else:
+                    conn.sendall(('Known Nodes Index is out of bounds!').encode('utf-8'))
+            elif (payload[0] == "b"):
+                print("detected b")
+                requestedNum = int.from_bytes(data, 'little', signed = False)
+                print(requestedNum)
+                conn.sendall(suggestPosts[requestedNum].encode('utf-8'))
